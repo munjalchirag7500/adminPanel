7cc46a83f1a28c276f4428874428a89e8c731356
munjalchirag7500 <munjalchirag7500@gmail.com>
2020-03-17 01:48:09 +0530

    qq

--- model/user.js
+++ model/user.js
@@ -3,63 +3,63 @@ const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt');
 const control = require('../controller/userController')
 SALT_WORK_FACTOR = 10;
-const validate=require('mongoose-validator')
+const validate = require('mongoose-validator')
 
 var nameValidator = [
     validate({
-      validator: 'isLength',
-      arguments: [3, 35],
-      message: 'Name should be between {ARGS[0]} and {ARGS[1]} characters',
+        validator: 'isLength',
+        arguments: [3, 3],
+        message: 'Name should be between {ARGS[0]} and {ARGS[1]} characters',
     }),
     validate({
-      validator: 'isAlphanumeric',
-      message: 'Name should contain alpha-numeric characters only',
+        validator: 'isAlphanumeric',
+        message: 'Name should contain alpha-numeric characters only',
     }),
-  ]
-  var ageValidator = [
+]
+var ageValidator = [
     validate({
-      validator: 'isLength',
-      arguments: [2, 3],
-      message: 'Age should be Above 18 and Below 100',
-    }),
-    validate({
-      validator: 'isNumeric',
-      message: 'Age should contain numeric characters only',
-    }),
-  ]
-  var passValidator = [
-    validate({
-      validator: 'isLength',
-      arguments: [8, 35],
-      message: 'Pawword Length Should Be Betwwen 8 and 32',
+        validator: 'isLength',
+        arguments: [2, 3],
+        message: 'Age should be Above 18 and Below 100',
     }),
     validate({
-      validator: 'isAlphanumeric',
-      message: 'Password should contain numeric characters only',
+        validator: 'isNumeric',
+        message: 'Age should contain numeric characters only',
     }),
-  ]
-  var emailValidator = [
+]
+var passValidator = [
     validate({
-      validator: 'isEmail',
-      message: 'Invalid Email',
+        validator: 'isLength',
+        arguments: [8, 35],
+        message: 'Pawword Length Should Be Betwwen 8 and 32',
     }),
-  ]
+    validate({
+        validator: 'isAlphanumeric',
+        message: 'Password should contain numeric characters only',
+    }),
+]
+var emailValidator = [
+    validate({
+        validator: 'isEmail',
+        message: 'Invalid Email',
+    }),
+]
 const UserSchema = new Schema({
-    name: { type: String ,validate:nameValidator},
-    age: { type: String,validate:ageValidator },
-    email: { type: String, unique: true ,validate:emailValidator},
-    password: {type:String,validate:passValidator},
+    name: { type: String, validate: nameValidator },
+    age: { type: String, validate: ageValidator },
+    email: { type: String, unique: true, validate: emailValidator },
+    password: { type: String, validate: passValidator },
     role: {
         type: String,
         enum: ['admin', 'subadmin', 'user'],
         default: 'user'
     },
     is_deleted: { type: Boolean, default: false },
-    passwordreset:String,
-    profilepic:String
+    passwordreset: String,
+    profilepic: String
 });
 
-UserSchema.pre('save',function (next) {
+UserSchema.pre('save', function (next) {
     var user = this;
 
 
